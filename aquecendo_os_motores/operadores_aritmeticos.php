3809ed489779ab16ae79735216e38db032ce9623
nely de faria <nelly.nf54@gmail.com>
2022-04-10 19:17:05 -0300

    Criando arquivo operadores_aritmeticos.php

---
+++ aquecendo_os_motores/operadores_aritmeticos.php
@@ -0,0 +1,10 @@
+<?php
+
+var_dump(10 + 10); // adição
+var_dump(10 - 10); // subtração
+var_dump(10 * 10); // multiplicação
+var_dump(10 / 10); // divisão
+var_dump(10 % 10); // resto
+
+var_dump(10 + 10 * 2); // regra de precedência
+var_dump((10 + 10) * 2); // organizando a precedência por parenteses
