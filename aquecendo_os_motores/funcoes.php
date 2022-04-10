a63c476558ed939ce750696129d8318f3a226bda
nely de faria <nelly.nf54@gmail.com>
2022-04-10 19:22:00 -0300

    Criando arquivo funcoes.php

---
+++ aquecendo_os_motores/funcoes.php
@@ -0,0 +1,15 @@
+<?php 
+
+var_dump(date('d/m/y'));
+var_dump(date('d/m/Y'));
+
+$numeros = [0, 1, 2];
+
+// echo count($numeros);
+
+function somar($n1, $n2)
+{
+	return $n1 + $n2;
+}
+
+echo somar(10, 15);
